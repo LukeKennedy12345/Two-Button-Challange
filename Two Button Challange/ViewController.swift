a5766599783a9d16988f2a265d18266a5d6d7981
Luke Kennedy <lkennedy3@zagmail.gonzaga.edu>
2022-01-26 14:16:02 -0800

    Initial Commit

---
+++ Two Button Challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Button Challange
+//
+//  Created by Lucas Kennedy on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
